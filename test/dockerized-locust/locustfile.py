733438edcaa43ff159bb6977d65575afa1e292b4
CaixiangFan <caixiang@ualberta.ca>
2021-03-30 15:15:01 -0600

    test locust shape working in swarm

--- test/dockerized-locust/locustfile.py
+++ test/dockerized-locust/locustfile.py
@@ -118,37 +118,37 @@ class IotaHttpUser(HttpUser):
         print(request.json())
 
 
-class StagesShape(LoadTestShape):
-    """
-    A simply load test shape class that has different user and spawn_rate at
-    different stages.
-    Keyword arguments:
-        stages -- A list of dicts, each representing a stage with the following keys:
-            duration -- When this many seconds pass the test is advanced to the next stage
-            users -- Total user count
-            spawn_rate -- Number of users to start/stop per second
-            stop -- A boolean that can stop that test at a specific stage
-        stop_at_end -- Can be set to stop once all stages have run.
-    """
+# class StagesShape(LoadTestShape):
+#     """
+#     A simply load test shape class that has different user and spawn_rate at
+#     different stages.
+#     Keyword arguments:
+#         stages -- A list of dicts, each representing a stage with the following keys:
+#             duration -- When this many seconds pass the test is advanced to the next stage
+#             users -- Total user count
+#             spawn_rate -- Number of users to start/stop per second
+#             stop -- A boolean that can stop that test at a specific stage
+#         stop_at_end -- Can be set to stop once all stages have run.
+#     """
 
-    stages = [
-        {"duration": 60, "users": 10, "spawn_rate": 10},
-        {"duration": 120, "users": 20, "spawn_rate": 10},
-        {"duration": 180, "users": 30, "spawn_rate": 10},
-        {"duration": 240, "users": 40, "spawn_rate": 10},
-        {"duration": 300, "users": 50, "spawn_rate": 10},
-        {"duration": 360, "users": 60, "spawn_rate": 10},
-        {"duration": 420, "users": 70, "spawn_rate": 10},
-        {"duration": 480, "users": 80, "spawn_rate": 10},
-        {"duration": 540, "users": 90, "spawn_rate": 10},
-        {"duration": 600, "users": 100, "spawn_rate": 10},
-    ]
+#     stages = [
+#         {"duration": 60, "users": 10, "spawn_rate": 10},
+#         {"duration": 120, "users": 20, "spawn_rate": 10},
+#         {"duration": 180, "users": 30, "spawn_rate": 10},
+#         {"duration": 240, "users": 40, "spawn_rate": 10},
+#         {"duration": 300, "users": 50, "spawn_rate": 10},
+#         {"duration": 360, "users": 60, "spawn_rate": 10},
+#         {"duration": 420, "users": 70, "spawn_rate": 10},
+#         {"duration": 480, "users": 80, "spawn_rate": 10},
+#         {"duration": 540, "users": 90, "spawn_rate": 10},
+#         {"duration": 600, "users": 100, "spawn_rate": 10},
+#     ]
 
-    def tick(self):
-        run_time = self.get_run_time()
+#     def tick(self):
+#         run_time = self.get_run_time()
 
-        for stage in self.stages:
-            if run_time < stage["duration"]:
-                tick_data = (stage["users"], stage["spawn_rate"])
-                return tick_data
-        return None
+#         for stage in self.stages:
+#             if run_time < stage["duration"]:
+#                 tick_data = (stage["users"], stage["spawn_rate"])
+#                 return tick_data
+#         return None
